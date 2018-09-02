828d4455ab08ab53b312e4e2b0ac6eed5c511af6
Liviu Ioan Iancu <LiviuLvu@users.noreply.github.com>
2018-09-03 00:34:16 +0300

    Generate ingredients based on data object

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,18 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 
 const Burger = (props) => {
+  // Generate ingredients dynamically
+  // The generated array of ingredients is based on the database object / current state
+  const ingredientLayers = Object.keys(props.ingredients).map(igKey => { // ex. cheese, bacon
+    return [...Array( props.ingredients[igKey] )].map((_, i) => { // number of layers for that ingredient
+      return <BurgerIngredient key={igKey + 1} type={igKey} />
+      });
+  });
+
   return (
     <div className={style.Burger}>
       <BurgerIngredient type={"bread-top"}/>
-      <BurgerIngredient type={"cheese"}/>
-      <BurgerIngredient type={"meat"}/>
+      { ingredientLayers }
       <BurgerIngredient type={"bread-bottom"}/>
     </div>
   );
