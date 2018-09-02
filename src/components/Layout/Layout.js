a26ea7ecd65d8d1b4bcf2628899a5fc93b902efc
Liviu Ioan Iancu <LiviuLvu@users.noreply.github.com>
2018-09-02 16:35:47 +0300

    Add dynamic ingredient component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import Aux from '../../hoc/Auxiliary';
+import style from './Layout.css';
 
 const Layout = (props) => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={style.Content}>
       {props.children}
     </main>
   </Aux>
