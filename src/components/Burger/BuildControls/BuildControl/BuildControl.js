d8f9b36d35853a0fac85a8a43f73b19ec0f018e9
LiviuLvu <liviuiancu@protonmail.com>
2018-09-04 20:30:56 +0300

    Add build controls files

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import style from './BuildControl.css';
+
+const BuildControl = () => {
+  return(
+    <div className={style.BuildControl}>
+      <div className={style.Label}>{props.label}</div>
+      <button className={style.Less}>Less</button>
+      <button className={style.More>More</button>
+    </div>
+  );
+};
+
+export default BuildControl;
---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import style from './BuildControls.css';
+
+const BuildControls = (props) => {
+    return (
+      <div>
+
+      </div>
+    );
+};
+
+export default BuildControls;
