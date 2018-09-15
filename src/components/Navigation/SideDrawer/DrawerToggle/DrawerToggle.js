d63d05155f4c38da9281782ae72bfa748b9abfc9
LiviuLvu <liviuiancu@protonmail.com>
2018-09-15 19:18:45 +0300

    Style toolbar drawer button

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
+import style from './DrawerToggle.css';
+
 const DrawerToggle = (props) =>
-  <div onClick={props.clicked}>MENU</div>;
+  <div
+    onClick={props.clicked}
+    className={style.DrawerToggle}
+  >
+    <div/>
+    <div/>
+    <div/>
+  </div>;
 
 export default DrawerToggle;
