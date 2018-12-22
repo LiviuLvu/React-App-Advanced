a4ef37ea1906de3a452bf80cf717c26f64afce41
Liviu Ioan Iancu <liviu.iancu@3pillarglobal.com>
2018-12-22 21:55:03 +0200

    Add orders page

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Order extends Component {
+  render() {
+    return (
+        <div>
+          <Order />
+          <Order />
+        </div>
+    );
+  }
+}
+
+export default Component;
