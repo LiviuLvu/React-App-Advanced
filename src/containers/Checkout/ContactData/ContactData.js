8510e1ad8dec05c31ca46b5d7fcedfd1bed2feaf
Liviu Ioan Iancu <liviu.iancu@3pillarglobal.com>
2018-12-30 11:20:38 +0200

    Adjust order actions

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,8 +92,7 @@ class ContactData extends Component {
         valid: true
       },
     },
-    formIsValid: false,
-    loading: false
+    formIsValid: false
   };
 
   orderHandler = (event) => {
@@ -183,7 +182,7 @@ class ContactData extends Component {
         >ORDER</Button>
       </form>
     );
-    if (this.state.loading === true) {
+    if (this.props.loading === true) {
       form = <Spinner />
     }
     return (
@@ -198,12 +197,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   }
 };
 
 const mapDispatchToProps = dispatch => {
-  onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData));
+  return {
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+  }
 };
 
-export default connect(mapStateToProps)( withErrorHandler(ContactData, axios) );
+export default connect(mapStateToProps, mapDispatchToProps)( withErrorHandler(ContactData, axios) );
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -4,5 +4,5 @@ export {
   initIngredients
 } from './burgerBuilderAction';
 export {
-  purchaseBurgerStart
+  purchaseBurger
 } from './order';
