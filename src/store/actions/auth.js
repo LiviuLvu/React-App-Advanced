841e9960da8eeef3a1f3bc2736282db23b2ec0b0
Liviu Ioan Iancu <liviu.iancu@3pillarglobal.com>
2018-12-31 12:45:28 +0200

    Signup new user

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authStart = () => {
@@ -23,5 +25,20 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart());
+    const firebaseEndpointURL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCgvP6e1WH7aa2jTcF-FJlBiSaVjv8alsE';
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios.post(firebaseEndpointURL, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail());
+      });
   }
 };
