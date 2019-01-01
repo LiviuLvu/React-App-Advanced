e60b6ced8fcd59b3c46fe305a9c511af6410359d
Liviu Ioan Iancu <liviu.iancu@3pillarglobal.com>
2019-01-01 19:44:34 +0200

    Redirect user to checkout page

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -43,7 +43,13 @@ class Auth extends Component {
     isSignup: true
   };
 
-  checkValidity(value, rules) {
+  componentDidMount () {
+    if (!this.props.buildingBurger && this.props.authRedirectPath !== '/') {
+      this.props.onSetAuthRedirectPath();
+    }
+  }
+
+  checkValidity (value, rules) {
     let isValid = true;
 
     if (rules.required) {
@@ -126,7 +132,7 @@ class Auth extends Component {
 
     let authRedirect = null;
     if (this.props.isAuth) {
-      authRedirect = <Redirect to="/" />
+      authRedirect = <Redirect to={this.props.authRedirectPath} />
     }
 
     return (
@@ -150,13 +156,16 @@ const mapStateToProps = state => {
   return {
     loading: state.auth.loading,
     error: state.auth.error,
-    isAuth: state.auth.token !== null
+    isAuth: state.auth.token !== null,
+    buildingBurger: state.burgerBuilder.building,
+    authRedirectPath: state.auth.authRedirectPath
   }
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
+    onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+    onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
   }
 };
 
